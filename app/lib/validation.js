84e89c14d6e5b282e150c3c4c50bb4e82dca9b06
Sofiia Vedenieva <sofiia.vedenieva@student.griffith.ie>
2026-04-19 21:17:03 +0100

    input validation check for User and Appliance fields

--- app/lib/validation.js
+++ app/lib/validation.js
@@ -11,3 +11,40 @@ export const brandRegex = /^[A-Za-z0-9\s\-&.]{1,50}$/;
 export const modelNumberRegex = /^[A-Za-z0-9\-_/.\s]{1,50}$/;
 export const serialNumberRegex = /^[A-Za-z0-9\-_]{3,50}$/;
 export const costRegex = /^\d{1,7}(\.\d{1,2})?$/;
+
+//implementing validation for inputs using regex 
+export function validateUser(data) {
+  if (!nameRegex.test(data.firstName))
+    return { valid: false, message: 'Invalid first name.Plaease, enter from 2–50 letters' };
+  if (!nameRegex.test(data.lastName))
+    return { valid: false, message: 'Invalid last name Plaease, enter from 2–50 letters' };
+  if (!addressRegex.test(data.address))
+    return { valid: false, message: 'Invalid address. Plaease, enter from 5–150 characters' };
+  if (!mobileRegex.test(data.mobile))
+    return { valid: false, message: 'Invalid mobile . Plaease, enter input in valid format:08X XXXXXXX' };
+  if (!emailRegex.test(data.email))
+    return { valid: false, message: 'Invalid email address. Please, enter again' };
+  if (!eircodeRegex.test(data.eircode))
+    return { valid: false, message: 'Invalid Eircode format .Plaease, enter input in valid format:D00 AB00' };
+  return { valid: true };
+}
+
+export function validateAppliance(data) {
+  if (!applianceTypeRegex.test(data.applianceType))
+    return { valid: false, message: 'Invalid appliance type .Please enter letters only' };
+  if (!brandRegex.test(data.brand))
+    return { valid: false, message: 'Invalid brand name. Please, try again' };
+  if (!modelNumberRegex.test(data.modelNumber))
+    return { valid: false, message: 'Invalid model number. Plaese, try again' };
+  if (!serialNumberRegex.test(data.serialNumber))
+    return { valid: false, message: 'Invalid serial number. Please enter in alphanumeric, from 3–50 characters' };
+  if (!data.purchaseDate)
+    return { valid: false, message: 'Please, enter the purchase date' };
+  if (!data.warrantyExpirationDate)
+    return { valid: false, message: 'Please, enter warranty expiration date' };
+  if (new Date(data.warrantyExpirationDate) <= new Date(data.purchaseDate))
+    return { valid: false, message: 'Warranty expiration must be after purchase date' };
+  if (!costRegex.test(String(data.cost)))
+    return { valid: false, message: 'Invalid cost. Please, enter in format : 000.00' };
+  return { valid: true };
+}
