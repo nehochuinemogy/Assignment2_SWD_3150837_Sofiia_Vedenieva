55832334f86410a933e12417744a28b7c35f1d43
Sofiia Vedenieva <sofiia.vedenieva@student.griffith.ie>
2026-04-20 18:00:44 +0100

    Appliance type check

--- app/api/appliances/add/route.js
+++ app/api/appliances/add/route.js
@@ -24,6 +24,33 @@ export async function POST(request) {
       cost: body.cost,
     };
 
-    
+    //server-side validation
+const userCheck = validateUser(clean);
+    if (!userCheck.valid) {
+      return NextResponse.json({ message: userCheck.message }, { status: 400 });
+    }
+
+    const appCheck = validateAppliance(clean);
+    if (!appCheck.valid) {
+      return NextResponse.json({ message: appCheck.message }, { status: 400 });
+    }
+
+    //database operations
+    const conn = await pool.getConnection();
+
+        try {
+      // checking if serial number already exists
+      //and if in code is no duplicates
+      const [existing] = await conn.execute(
+        'SELECT ApplianceID FROM Appliances WHERE SerialNumber = ?',
+        [clean.serialNumber]
+      );
+
+      //if duplicate exists
+      if (existing.length > 0) {
+        conn.release();
+        return NextResponse.json({ message: 'You entered extsting appliance' }, { status: 409 });
+      }
+
 }
 };
