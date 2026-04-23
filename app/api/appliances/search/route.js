4d9f1c610b07100804e3b04d93fea008438ba240
Sofiia Vedenieva <sofiia.vedenieva@student.griffith.ie>
2026-04-23 15:42:16 +0100

    Added serial numver validation to GET

--- app/api/appliances/search/route.js
+++ app/api/appliances/search/route.js
@@ -1,4 +1,16 @@
 import { NextResponse } from 'next/server';
-import pool from '../../../../lib/db';
 import { serialNumberRegex, sanitise } from '../../../../lib/validation'
 
+export async function GET(request) {
+  try {
+    // getting queryy url
+    const { searchParams } = new URL(request.url);
+    // extracting serial number 
+    const serial = sanitise(searchParams.get('serial') || '');
+
+    // validating serial number format before hitting database
+    if (!serialNumberRegex.test(serial)) {
+      return NextResponse.json({ message: 'invalid serial number.please, try again' }, { status: 400 });
+    }
+  }
+}
