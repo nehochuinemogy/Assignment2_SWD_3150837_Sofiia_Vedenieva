6b6f05a42df57574da3a23a65bccde6e974c0f44
Sofiia Vedenieva <sofiia.vedenieva@student.griffith.ie>
2026-04-23 15:57:09 +0100

    Added serial number search with joining user function

--- app/api/appliances/search/route.js
+++ app/api/appliances/search/route.js
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server';
-import { serialNumberRegex, sanitise } from '../../../../lib/validation'
+import pool from '../lib/db';
+import { serialNumberRegex, sanitise } from './lib/validation';
 
 export async function GET(request) {
   try {
@@ -10,7 +11,32 @@ export async function GET(request) {
 
     // validating serial number format before hitting database
     if (!serialNumberRegex.test(serial)) {
-      return NextResponse.json({ message: 'invalid serial number.please, try again' }, { status: 400 });
+      return NextResponse.json({ message: 'invalid serial number' }, { status: 400 });
     }
+  // JOIN querry to joijn appliance and user
+    //using ? avoiding sql injection
+    const [rows] = await pool.execute(
+      `SELECT a.*, u.FirstName, u.LastName, u.Address, u.Mobile, u.Email, u.Eircode
+       FROM Appliances a
+       JOIN Users u ON a.UserID = u.UserID
+       WHERE a.SerialNumber = ?`,
+      [serial]
+    );
+
+    // if no rows returned, appliance does not exist
+    if (rows.length === 0) {
+      return NextResponse.json({ 
+        message: 'Appliance not found' 
+    }, { 
+        status: 404 
+    });
+    }
+
+    // returning matching appliance
+    return NextResponse.json({ appliance: rows[0] }, { status: 200 });
+  } catch (error) {
+    console.error('Search error:', error);
+    return NextResponse.json({ message: 'Server error' }, { status: 500 });
   }
+
 }
